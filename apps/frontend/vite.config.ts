ff8f48ded67fcacf5dd4da421836f5ecbd9cfdfc
Marc Fernandez <mcfdez87@gmail.com>
2026-03-07 20:39:25 +0100

    docs: add CONTRIBUNTING

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,19 +1,17 @@
 import path from 'path';
 
 import react from '@vitejs/plugin-react-swc';
-import { componentTagger } from 'lovable-tagger';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     server: {
-        host: '::',
-        port: 8080,
+        port: 3000,
         hmr: {
             overlay: false,
         },
     },
-    plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+    plugins: [react()].filter(Boolean),
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),

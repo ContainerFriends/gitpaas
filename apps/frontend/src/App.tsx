2a309743b948c9c7fd4680d258e18c38d2cdadb4
Marc Fernandez <mcfdez87@gmail.com>
2026-03-08 17:31:17 +0100

    feat: add toasts

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { DashboardPage } from '@pages/DashboardPage';
 import { NotFoundPage } from '@pages/NotFoundPage';
 import { ProjectsPage } from '@pages/ProjectsPage';
+import { Toaster } from '@shared/components/sonner';
 import { TooltipProvider } from '@shared/components/tooltip';
 
 const queryClient = new QueryClient();
@@ -15,15 +17,18 @@ const queryClient = new QueryClient();
 export function App(): ReactNode {
     return (
         <QueryClientProvider client={queryClient}>
-            <TooltipProvider>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<DashboardPage />} />
-                        <Route path="/projects" element={<ProjectsPage />} />
-                        <Route path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </BrowserRouter>
-            </TooltipProvider>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+                <TooltipProvider>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<DashboardPage />} />
+                            <Route path="/projects" element={<ProjectsPage />} />
+                            <Route path="*" element={<NotFoundPage />} />
+                        </Routes>
+                    </BrowserRouter>
+                    <Toaster />
+                </TooltipProvider>
+            </ThemeProvider>
         </QueryClientProvider>
     );
 }

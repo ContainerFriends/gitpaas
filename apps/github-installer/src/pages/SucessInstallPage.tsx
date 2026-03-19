78acfd4fd24e462235fca94a46b376a7fdad62c8
Marc Fernandez <mcfdez87@gmail.com>
2026-03-19 10:58:27 +0100

    feat: add success installation page

---
+++ apps/github-installer/src/pages/SucessInstallPage.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+import { SuccessInstallContainer } from '@features/install/ui/containers/SucessInstallContainer';
+
+/**
+ * Success install page component.
+ */
+export function SuccessInstallPage(): ReactNode {
+    return <SuccessInstallContainer />;
+}

a372f4ab693f1b98120e63c66ecd207491e125a3
Marc Fernandez <mcfdez87@gmail.com>
2026-03-21 16:26:43 +0100

    fix: strip version to for Docker images on installation

--- packages/setup/utils/clean-version-for-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clean version tag for Docker images (remove 'v' prefix if present)
-clean_version_for_docker() {
-    local version="$1"
-    # Remove 'v' prefix only if followed by digits (e.g., v1.3.0 -> 1.3.0)
-    echo "$version" | sed 's/^v\([0-9]\)/\1/'
-}
